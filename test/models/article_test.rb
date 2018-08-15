515f131336d68e6f275e59838c85633bf5f465c0
Computer <computer@Computers-MacBook-Air.local>
2018-08-15 14:34:46 -0700

    Added test for article, comments, and models

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save article without title" do
+    article = Article.new
+    assert_not article.save, "Saved the article without a title"
+  end
+
+  test "should report error" do
+    #some_undefined_variable is not defined elsewhere in the test case
+    assert_raises(NameError) do # this line make the next line to pass the test
+    some_undefined_variable
+    assert true
+    end
+  end
+
 end
